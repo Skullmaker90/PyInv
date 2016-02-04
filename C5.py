30b754bc1bb90e63a6146a31553372de12cc25ff
Anthony Smith <asmith@cari.net>
2016-02-04 06:32:47 -0800

    Further reduction.

--- C5.py
+++ C5.py
@@ -8,11 +8,9 @@ import Dbquery
 from Mydbcon import Mydbcon
 
 def main(com):
-	date = datetime.date.today()
-	date = date.isoformat()
+	date = datetime.date.today().isoformat()
 	q = Dbquery.handler(com)(sys.argv[2:], date)
 	result = Mydbcon().query(q)
-	#result = con.query(q)
 	if result:	
 		for i in result:
 			print("Location: " + i[0] + " | Switchport: " + i[1] + " | Date: " + i[2].isoformat() + "\n"
@@ -20,7 +18,7 @@ def main(com):
 	if (len(sys.argv) > 2):
 		v = Dbquery.handler('verify')(sys.argv[2])
 		for i in Mydbcon().query(v):
-			print('\nLocation: ' + i[0] + ' | Open: ' + i[3] + '\n')
+			print('\nLocation: ' + i[0] + ' | Open: ' + i[3] + ' | Verify Date: ' + i[2].isoformat() + '\n')
 	
 
 if __name__ == '__main__':
