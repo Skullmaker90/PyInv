0f85e23b9e29244d7ad65063111992b8ecd7c9e1
Anthony Smith <asmith@cari.net>
2016-02-04 05:24:01 -0800

    Condensed code, removed unneccessary assignments.

--- C5.py
+++ C5.py
@@ -10,17 +10,18 @@ from Mydbcon import Mydbcon
 def main(com):
 	date = datetime.date.today()
 	date = date.isoformat()
-	q = Dbquery.handler(com)
-	q = q(sys.argv[2:], date)
-	con = Mydbcon()
-	result = con.query(q)
+	q = Dbquery.handler(com)(sys.argv[2:], date)
+	result = Mydbcon().query(q)
+	#result = con.query(q)
 	if result:	
 		for i in result:
 			print("Location: " + i[0] + " | Switchport: " + i[1] + " | Date: " + i[2].isoformat() + "\n"
 				+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	if (len(sys.argv) > 2):
+		v = Dbquery.handler('verify')(sys.argv[2])
+		for i in Mydbcon().query(v):
+			print('\nLocation: ' + i[0] + ' | Open: ' + i[3] + '\n')
 	
-def test(com):
-	print(com)
 
 if __name__ == '__main__':
 	import sys
