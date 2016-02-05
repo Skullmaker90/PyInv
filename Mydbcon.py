79e1f6e2b2ae334547c10765c0e0082ef468b9ef
Anthony Smith <asmith@cari.net>
2016-02-05 01:34:28 -0800

    Because black magic context managers.

--- Mydbcon.py
+++ Mydbcon.py
@@ -6,29 +6,30 @@
 
 import MySQLdb
 
+class Controller(object):
+	def __init__(self):
+		self.con = None
+	
+	def __enter__(self):
+		self.con = MySQLdb.connect(host='71.6.132.43',
+				user='cari',
+				passwd='',
+				db='C5Datacenter')
+		return self.con.cursor()
+	
+	def __exit__(self, type, value, traceback):
+		if self.con:
+			self.con.commit()
+			self.con.close()
+		else:
+			raise Exception("Something went wrong")
+
 class Mydbcon(object):
 	def __init__(self):
 		pass
 
-	def dbopen(self):
-		return MySQLdb.connect(host='71.6.132.43', 
-				user='cari',
-				passwd='',
-				db='C5Datacenter')
-
-	def close(self, dbcon):
-		if dbcon.close():
-			return True
-
 	def query(self, string):
-		try:
-			db = self.dbopen()
-			cur = db.cursor()
-			if cur.execute("""%s""" % (string,)):
-				result = cur.fetchall()
-				db.commit()
-				self.close(cur)
+		with Controller() as db:
+			if db.execute("""%s""" % (string,)):
+				result = db.fetchall()
 			return result
-				
-		except:
-			self.close(cur)
