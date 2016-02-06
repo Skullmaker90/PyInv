fc09e0193a85fc7a7e71ce0a242241cb1b605b32
Anthony Smith <asmith@cari.net>
2016-02-05 23:47:56 -0800

    Changed verify to lookup.

--- C5.py
+++ C5.py
@@ -11,12 +11,12 @@ def main(com):
 	date = datetime.date.today().isoformat()
 	q = Dbquery.handler(com)(sys.argv[2:], date)
 	result = Mydbcon().query(q)
-	if result:	
+	if result:
 		for i in result:
 			print("Location: " + i[0] + " | Switchport: " + i[1] + " | Date: " + i[2].isoformat() + "\n"
 				+ "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	if (len(sys.argv) > 2):
-		v = Dbquery.handler('verify')(sys.argv[2])
+		v = Dbquery.handler('lookup')(sys.argv[2])
 		for i in Mydbcon().query(v):
 			print('\nLocation: ' + i[0] + ' | Open: ' + i[3] + ' | Verify Date: ' + i[2].isoformat() + '\n')
 	
